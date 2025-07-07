b3849955e3e9d582663f66ebf0798d62ee9aede9
Leonardo Saraiva <113627176+Saraiva0dz9@users.noreply.github.com>
2025-07-07 11:12:39 -0300

    Feat: Atualizando script

--- Scripts/Tabela Atendimentos.sql
+++ Scripts/Tabela Atendimentos.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Atendimentos (
     ID INT PRIMARY KEY IDENTITY(1,1),
-    NumeroSequencial INT NOT NULL UNIQUE, -- Número único por atendimento (pode ser autoincremento ou gerado por regra de negócio)
+    NumeroSequencial INT NOT NULL, 
     PacienteID INT NOT NULL,
     DataHoraChegada DATETIME DEFAULT GETDATE(),
     Status NVARCHAR(20) CHECK (Status IN ('Aguardando', 'Em Triagem', 'Em Atendimento', 'Finalizado', 'Cancelado')),
